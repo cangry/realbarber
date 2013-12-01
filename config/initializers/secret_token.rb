1e887a33c9fb79667eb047ea95f0dd4e5e879eb1
cangry88 <carloslgonzalez88@gmail.com>
2013-11-30 21:21:41 -0500

    Created new project, added bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Realbarber::Application.config.secret_key_base = '3334a7d0b2fe4687541e34f5731dbcc05e5005683eeb9192c4ae0ba291918e6c5792bacb4633f575a72e8f2132d10770a7d34e18dfdb6dec9f5ca7ee218844ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Realbarber::Application.config.session_store :cookie_store, key: '_realbarber_session'
