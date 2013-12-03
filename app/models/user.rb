32f682f98cd90122c01566458a0c64877e79e143
Roberto C. Rivera <rivera.roberto89@gmail.com>
2013-12-03 01:29:13 -0500

    Devise authentication with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,26 @@
 class User < ActiveRecord::Base
+  # Virtual attribute for authenticating by either username or email
+	# This is in addition to a real persisted field like 'username'
+	attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :barber
+
+  validates :username,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+  	conditions = warden_conditions.dup
+  	if login = conditions.delete(:login)
+  		where(conditions).where(["username = :value OR lower(email) = lower(:value)",{ :value => login }]).first
+    else
+      where(conditions).first
+    end
+   end
 end
