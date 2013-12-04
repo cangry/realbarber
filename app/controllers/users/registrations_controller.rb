d3dedfa0f9555776bb22c9580d5f5d9b86291d44
Roberto C. Rivera <rivera.roberto89@gmail.com>
2013-12-04 01:18:53 -0500

    Improve edit barber profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,29 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      #Here we can check if the registration is for Barber or Customer
+      #Create empty barber
+      Barber.create! :user_id => resource.id
+
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def update
     # For Rails 4
     account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
@@ -26,6 +51,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def edit
     #passes value to devise/registrations/edit.html.erd 
-    @barber = Barber.where("user_id = ?", current_user.id)
+    @barber = Barber.find_by user_id: current_user.id
+
   end
 end
