88ac642e2e22d3741aa85ad69d2720c6fdec081e
cangry88 <carloslgonzalez88@gmail.com>
2013-12-04 01:43:07 -0500

    added is_barber:boolean column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203172547) do
+ActiveRecord::Schema.define(version: 20131204063533) do
 
   create_table "barber_types", force: true do |t|
     t.integer  "barber_id"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 20131203172547) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "is_barber"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
